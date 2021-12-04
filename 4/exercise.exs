2d94ead7fe8a978a641f99246761c837c3d5e445
Bernardes <nuno.jmbernardes@gmail.com>
2021-12-04 15:11:07 +0000

    day 4 part 2

--- 4/exercise.exs
+++ 4/exercise.exs
@@ -1,7 +1,7 @@
 defmodule Exercise do
   def parse_input() do
     [drawn_numbers, _ | boards] =
-      File.read!("input2.txt")
+      File.read!("input.txt")
       |> String.split("\n")
 
     boards =
@@ -13,32 +13,39 @@ defmodule Exercise do
     {drawn_numbers |> String.split(","), boards}
   end
 
+  def bingo({drawn_numbers, boards}, _) when drawn_numbers == [] or boards == [],
+    do: IO.puts("Game ended :D")
+
   def bingo({[n1, n2, n3, n4, n5 | drawn_numbers], boards}, []) do
     past_numbers = [n1, n2, n3, n4, n5]
 
-    boards
-    |> Enum.map(fn board ->
-      case won?(winning_combinations(board), past_numbers) do
-        true -> winner(board, past_numbers)
-        false -> nil
-      end
-    end)
+    playing_boards =
+      boards
+      |> Enum.map(fn board ->
+        case won?(winning_combinations(board), past_numbers) do
+          true -> winner(board, past_numbers)
+          false -> board
+        end
+      end)
+      |> Enum.reject(&is_nil(&1))
 
-    bingo({drawn_numbers, boards}, [n1, n2, n3, n4, n5])
+    bingo({drawn_numbers, playing_boards}, [n1, n2, n3, n4, n5])
   end
 
   def bingo({[curr_number | drawn_numbers], boards}, past_numbers) do
     past_numbers = past_numbers ++ [curr_number]
 
-    boards
-    |> Enum.map(fn board ->
-      case won?(winning_combinations(board), past_numbers) do
-        true -> winner(board, past_numbers)
-        false -> nil
-      end
-    end)
+    playing_boards =
+      boards
+      |> Enum.map(fn board ->
+        case won?(winning_combinations(board), past_numbers) do
+          true -> winner(board, past_numbers)
+          false -> board
+        end
+      end)
+      |> Enum.reject(&is_nil(&1))
 
-    bingo({drawn_numbers, boards}, past_numbers)
+    bingo({drawn_numbers, playing_boards}, past_numbers)
   end
 
   def winner(board, past_numbers) do
@@ -47,7 +54,7 @@ defmodule Exercise do
       |> Enum.sum()) * String.to_integer(List.last(past_numbers)))
     |> IO.inspect()
 
-    exit(:shutdown)
+    nil
   end
 
   def winning_combinations(board) do
