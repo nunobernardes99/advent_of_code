c9dc8aa47996e31f6031d5d938e769b6b210c200
Bernardes <nuno.jmbernardes@gmail.com>
2021-12-11 22:37:20 +0000

    feat: day 9 part 2

--- 9/exercise.exs
+++ 9/exercise.exs
@@ -13,6 +13,7 @@ defmodule Exercise do
     lists
     |> Enum.with_index()
     |> Enum.map(fn {row_list, y_coord} -> row(row_list, y_coord) end)
+    |> List.flatten()
   end
 
   def row(row_list, row_index) do
@@ -43,16 +44,69 @@ defmodule Exercise do
   defp lowest_point?(true, value), do: value + 1
   defp lowest_point?(_, _), do: 0
 
-  defp neighbours(x_coord, y_coord),
-    do: [
-      {x_coord - 1, y_coord},
-      {x_coord, y_coord - 1},
-      {x_coord + 1, y_coord},
-      {x_coord, y_coord + 1}
-    ]
+  def part2(grid) do
+    {last_group, basins} =
+      find_low_points(grid)
+      |> Enum.reduce({grid, []}, fn low_point, {non_visited, basins} ->
+        basin =
+          find_basin(non_visited, low_point)
+          |> List.flatten()
+          |> Enum.uniq()
+
+        {non_visited -- basin, basins ++ [basin]}
+      end)
+
+    (basins ++ [last_group])
+    |> Enum.map(fn basin ->
+      basin
+      |> Enum.reject(fn {v, _, _} -> v == 9 end)
+      |> Enum.count()
+    end)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.product()
+  end
+
+  defp find_basin(grid, curr, basin \\ [])
+  defp find_basin(_grid, {9, x, y}, basin), do: basin ++ [{9, x, y}]
+
+  defp find_basin(grid, {_v, x, y}, basin) do
+    adjacents = adjacents(grid, x, y)
+
+    adjacents
+    |> Enum.map(&find_basin(grid -- [&1], &1, basin ++ [&1]))
+  end
+
+  defp find_low_points(grid) do
+    grid
+    |> Enum.reduce([], fn {v, x, y}, low_points ->
+      {adj_v, _adj_x, _adj_y} =
+        adjacents(grid, x, y)
+        |> Enum.min_by(fn {v, _, _} -> v end, fn -> {10, 10, 10} end)
+
+      case adj_v > v do
+        true -> low_points ++ [{v, x, y}]
+        false -> low_points
+      end
+    end)
+  end
+
+  defp adjacents(grid, x1, y1) do
+    neighbours = neighbours(x1, y1)
+
+    grid
+    |> Enum.filter(fn {_, x2, y2} -> {x2, y2} in neighbours end)
+  end
+
+  defp neighbours(x, y), do: [{x, y + 1}, {x, y - 1}, {x + 1, y}, {x - 1, y}]
 end
+
+# Exercise.parse_input()
+# |> Exercise.grid()
+# |> Exercise.part1()
+# |> IO.inspect()
 
 Exercise.parse_input()
 |> Exercise.grid()
-|> Exercise.part1()
+|> Exercise.part2()
 |> IO.inspect()
